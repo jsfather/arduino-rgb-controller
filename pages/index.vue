c46e5ba6bd477fa669ecce68e8c0bc1946c621cc
jsfather <keyvanmatinrad@outlook.com>
2024-09-19 14:16:10 +0330

    get and post built-in led status and interval added

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,45 @@
 <script setup lang="ts">
-const toastStore = useToastStore()
+const indexStore = useIndexStore()
 
+await callOnce(indexStore.getBuiltInLed)
 
+function toggleBuiltInLed() {
+  if (indexStore.$state.builtInLed === 'off') {
+    indexStore.setBuiltInLed({"led": 'on'})
+  } else {
+    indexStore.setBuiltInLed({"led": 'off'})
+  }
+}
+
+const currentTime = ref('')
+
+const $moment = useNuxtApp().$moment
+
+const targetTime = $moment('14:12:30', 'HH:mm:ss')
+
+const checkTime = (now: string) => {
+  if (now === targetTime.format('HH:mm:ss')) {
+    indexStore.setBuiltInLed({"led": 'on'})
+  }
+}
+
+setInterval(() => {
+  currentTime.value = $moment().format('HH:mm:ss')
+}, 1000)
+
+watch(currentTime, (newTime) => {
+  checkTime(newTime)
+})
 </script>
 <template>
-  <div @click="toastStore.addToast({type: 'info' , message: 'hello world'})">index</div>
+  <div class="flex justify-center p-4">
+    <div class="max-w-md p-6 bg-slate-700 rounded-lg shadow">
+      <Icon
+          :name="indexStore.$state.builtInLed ==='off' ? 'material-symbols:lightbulb-outline-rounded' : 'material-symbols:lightbulb-rounded'"
+          size="50"
+          class="text-white cursor-pointer" @click="toggleBuiltInLed"></Icon>
+      <p class="text-white">Current Time: {{ currentTime }}</p>
+    </div>
+
+  </div>
 </template>
