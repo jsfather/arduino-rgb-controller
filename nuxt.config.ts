347471806d0c24e1e6869a531e13c4fd35f07dce
jsfather <keyvanmatinrad@outlook.com>
2024-09-21 10:50:42 +0330

    ssr added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
-    ssr: false,
     modules: ['@nuxtjs/tailwindcss', 'nuxt-headlessui', "@nuxt/icon", "@pinia/nuxt", '@formkit/auto-animate/nuxt', "nuxt-lodash", '@vueuse/nuxt',],
     css: ['@/assets/css/main.css'],
     headlessui: {
@@ -9,7 +8,7 @@ export default defineNuxtConfig({
     },
     nitro: {
         devProxy: {
-            "/api": {
+            "/arduino": {
                 target: "http://192.168.1.14",
                 changeOrigin: true,
                 prependPath: true,
