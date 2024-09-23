867472367fe8181e0039ca1799c8685969db7688
jsfather <keyvanmatinrad@outlook.com>
2024-09-23 18:32:19 +0330

    led state type changed to 0 and 1

--- server/api/tasks.ts
+++ server/api/tasks.ts
@@ -5,20 +5,20 @@ import axios from 'axios';
 import schedule from 'node-schedule';
 import moment from 'moment';
 
-let tasks: Task[] = [{time: '09:30:00', led: 'on', color: '#FF0000'},
-    {time: '10:15:00', led: 'on', color: '#00FF00'},
-    {time: '10:30:00', led: 'on', color: '#FF0000'},
-    {time: '11:15:00', led: 'on', color: '#00FF00'},
-    {time: '11:30:00', led: 'on', color: '#FF0000'},
-    {time: '12:15:00', led: 'on', color: '#00FF00'},
-    {time: '13:30:00', led: 'on', color: '#FF0000'},
-    {time: '14:15:00', led: 'on', color: '#00FF00'},
-    {time: '14:30:00', led: 'on', color: '#FF0000'},
-    {time: '15:15:00', led: 'on', color: '#00FF00'},
-    {time: '15:30:00', led: 'on', color: '#FF0000'},
-    {time: '16:15:00', led: 'on', color: '#00FF00'},
-    {time: '16:30:00', led: 'on', color: '#FF0000'},
-    {time: '17:00:00', led: 'on', color: '#00FF00'},
+let tasks: Task[] = [{time: '09:30:00', led: 1, color: '#FF0000'},
+    {time: '10:15:00', led: 1, color: '#00FF00'},
+    {time: '10:30:00', led: 1, color: '#FF0000'},
+    {time: '11:15:00', led: 1, color: '#00FF00'},
+    {time: '11:30:00', led: 1, color: '#FF0000'},
+    {time: '12:15:00', led: 1, color: '#00FF00'},
+    {time: '13:30:00', led: 1, color: '#FF0000'},
+    {time: '14:15:00', led: 1, color: '#00FF00'},
+    {time: '14:30:00', led: 1, color: '#FF0000'},
+    {time: '15:15:00', led: 1, color: '#00FF00'},
+    {time: '15:30:00', led: 1, color: '#FF0000'},
+    {time: '16:15:00', led: 1, color: '#00FF00'},
+    {time: '16:30:00', led: 1, color: '#FF0000'},
+    {time: '17:00:00', led: 1, color: '#00FF00'},
 ];
 
 const scheduleTasks = () => {
@@ -29,7 +29,7 @@ const scheduleTasks = () => {
         if (taskTime.isAfter(now)) {
             schedule.scheduleJob(taskTime.toDate(), async () => {
                 try {
-                    await axios.post('http://192.168.31.169/built-in-led/set', {led: task.led});
+                    await axios.post('http://192.168.31.169/set', {led: task.led, color: task.color});
                 } catch (error) {
                     console.error(`Failed to execute task at ${task.time}:`, error);
                 }
